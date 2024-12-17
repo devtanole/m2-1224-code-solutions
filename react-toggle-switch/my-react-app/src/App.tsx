d6d8055bee13160b67f3951752ebaf8d495be20c
Devin Olesen <devintanielu@gmail.com>
2024-12-17 09:35:30 +0000

    move to ToggleSwitch file and on/off label added

--- react-toggle-switch/my-react-app/src/App.tsx
+++ react-toggle-switch/my-react-app/src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react';
-import './App.css';
-import './ToggleSwitch.css';
+import { ToggleSwitch } from './ToggleSwitch';
 
 function App() {
-  const [toggled, setToggle] = useState(false);
   return (
     <>
-      <button
-        className={`button ${toggled ? 'toggled' : ''}`}
-        onClick={() => setToggle(!toggled)}>
-        <div className="box"></div>
-      </button>
+      <ToggleSwitch />
     </>
   );
 }
