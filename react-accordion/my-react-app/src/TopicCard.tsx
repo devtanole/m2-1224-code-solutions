2d2fb60f31829ebc1a53e6b918a93585175cb23f
Devin Olesen <devintanielu@gmail.com>
2025-01-07 19:47:07 +0000

    update css

--- react-accordion/my-react-app/src/TopicCard.tsx
+++ react-accordion/my-react-app/src/TopicCard.tsx
@@ -7,9 +7,9 @@ type TopicCardProps = {
 
 export function TopicCard({ topic, handleClick, selected }: TopicCardProps) {
   return (
-    <div onClick={() => handleClick(topic.id)}>
+    <div onClick={() => handleClick(topic.id)} className="topic-card">
       <h2>{topic.title}</h2>
-      {selected ? <div>{topic.content}</div> : null}
+      {selected ? <div className="topic-content">{topic.content}</div> : null}
     </div>
   );
 }
