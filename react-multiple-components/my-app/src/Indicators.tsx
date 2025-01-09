1128072eb5b35d1a717ef5c59ac72ab3c32ed305
Devin Olesen <devintanielu@gmail.com>
2025-01-09 06:16:03 +0000

    phase 2 fin

--- react-multiple-components/my-app/src/Indicators.tsx
+++ react-multiple-components/my-app/src/Indicators.tsx
@@ -1,11 +1,21 @@
 type Props = {
   count: number;
+  handleSelect: (i: number) => void;
+  index: number;
 };
 
-export function Indicators({ count }: Props) {
+export function Indicators({ count, handleSelect, index }: Props) {
   const buttons = [];
   for (let i = 0; i < count; i++) {
-    buttons.push(<button key={i}>{i}</button>);
+    const selected = i === index;
+    buttons.push(
+      <button
+        className={selected ? 'selected' : ''}
+        key={i}
+        onClick={() => handleSelect(i)}>
+        {i}
+      </button>
+    );
   }
   return <>{buttons}</>;
 }
--- react-multiple-components/my-app/src/RotatingBanner.tsx
+++ react-multiple-components/my-app/src/RotatingBanner.tsx
@@ -1,20 +1,41 @@
 import { Banner } from './Banner';
 import { NavButton } from './NavButton';
 import { Indicators } from './Indicators';
+import { useState } from 'react';
 
 type BannerProps = {
   items: string[];
 };
 
 export function RotatingBanner({ items }: BannerProps) {
+  const [index, setIndex] = useState(0);
+
+  function handlePrev() {
+    if (index === 0) setIndex(items.length - 1);
+    else setIndex((prev) => prev - 1);
+  }
+
+  function handleNext() {
+    if (index === items.length - 1) setIndex(0);
+    else setIndex((prev) => prev + 1);
+  }
+
+  function handleSelect(i: number) {
+    setIndex(i);
+  }
+
   return (
     <>
-      <Banner name={items[4]} />
-      <NavButton label="Prev" />
+      <Banner name={items[index]} />
+      <NavButton label="Prev" handleClick={handlePrev} />
       <br></br>
-      <Indicators count={items.length} />
+      <Indicators
+        count={items.length}
+        handleSelect={handleSelect}
+        index={index}
+      />
       <br></br>
-      <NavButton label="Next" />
+      <NavButton label="Next" handleClick={handleNext} />
     </>
   );
 }
