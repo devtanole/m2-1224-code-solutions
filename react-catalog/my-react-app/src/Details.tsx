deaaa14bf4b57fe9b7b9233daa7cdf71430eff86
Devin Olesen <devintanielu@gmail.com>
2025-01-12 11:28:35 +0000

    css fin, button made, nav to cat later

--- react-catalog/my-react-app/src/Details.tsx
+++ react-catalog/my-react-app/src/Details.tsx
@@ -36,9 +36,50 @@ export function Details() {
       </div>
     );
   }
-  const { name, price, imageUrl, longDescription } = product;
+  const { name, price, imageUrl, longDescription, shortDescription } = product;
   return (
     <div className="container">
+      <div className="flex flex-col">
+        <div className="flex-auto p-6">
+          <Link
+            to="/"
+            className=" flex space-x-4 p-3 text-gray-600 cursor-pointer text-left">
+            &lt; Back to Catalog
+          </Link>
+          <div className="flex flex-wrap mb-4">
+            <div className="w-full sm:w-1/2 md:w-2/5 pt-2 px-4 flex items-stretch">
+              <img
+                src={imageUrl}
+                alt={name}
+                className="w-full h-full object-contain"
+              />
+            </div>
+            <div className="w-full sm:w-1/2 md:w-3/5 px-4">
+              <h2 className="text-left font-bold">{name}</h2>
+              <p className=" text-left text-xl text-gray-700 font-semibold mt-2">
+                {toDollars(price)}
+              </p>
+              <p className="text-left text-gray-700 mt-2">{shortDescription}</p>
+            </div>
+          </div>
+          <div className="px-4 mt-4">
+            <p className="text-left whitespace-pre-wrap">{longDescription}</p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-start mt-4 px-4 mt-4">
+        <button
+          onClick={() => alert(`added ${name} to cart`)}
+          className="px-4 py-2 rounded">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+/*
+<div className="container">
       <div className="flex flex-col">
         <div className="flex-auto p-6">
           <Link
@@ -59,11 +100,13 @@ export function Details() {
               {toDollars(price)}
             </p>
           </div>
+          <div>
+              <p className='px-4 whitespace-pre-wrap'>{shortDescription}</p>
+            </div>
           <div className="px-4">
             <p className="whitespace-pre-wrap">{longDescription}</p>
           </div>
         </div>
       </div>
     </div>
-  );
-}
+*/
