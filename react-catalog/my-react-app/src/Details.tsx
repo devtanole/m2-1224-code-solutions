8b8fc9972f2f4f77ca2e4897b4f1913c5ebede1a
Devin Olesen <devintanielu@gmail.com>
2025-01-13 18:36:36 +0000

    f

--- react-catalog/my-react-app/src/Details.tsx
+++ react-catalog/my-react-app/src/Details.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
 import { type Product } from '../lib/data';
+import { useEffect, useState } from 'react';
 import { readProduct } from '../lib/read';
 import { useParams, Link } from 'react-router-dom';
 import { toDollars } from '../lib/to-dollars';
@@ -79,34 +79,5 @@ export function Details() {
 }
 
 /*
-<div className="container">
-      <div className="flex flex-col">
-        <div className="flex-auto p-6">
-          <Link
-            to="/"
-            className="p-3 text-gray-600 cursor-pointer flex space-x-4">
-            &lt; Back to Catalog{''}
-          </Link>
-          <div className="flex flex-wrap mb-4">
-            <div className="w-full sm:w-1/2 md:w-2/5 pt-2 px-4">
-              <img
-                src={imageUrl}
-                alt={name}
-                className="w-full h-80 object-contain"
-              />
-            </div>
-            <h2 className="w-full sm:w-1/2 md:w-3/5 px-4 font-bold">{name}</h2>
-            <p className="text-xl text-gray-700 font-semibold">
-              {toDollars(price)}
-            </p>
-          </div>
-          <div>
-              <p className='px-4 whitespace-pre-wrap'>{shortDescription}</p>
-            </div>
-          <div className="px-4">
-            <p className="whitespace-pre-wrap">{longDescription}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+
 */
