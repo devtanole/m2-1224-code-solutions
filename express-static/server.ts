90f4f211e7907ebd154a5b1b04585df98a963f15
Devin Olesen <devintanielu@gmail.com>
2025-02-07 02:48:59 +0000

    update

--- express-static/server.ts
+++ express-static/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('got /!');
 });
@@ -9,8 +11,6 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
   res.send('got notes!');
 });
-
-app.use(express.static('public'));
 
 app.listen(8080, () => {
   console.log('Express server listening on port 8080');
