d4ab62b7e85c9b1d44b3f0e08f449ec443560b1f
Devin Olesen <devintanielu@gmail.com>
2025-02-28 02:49:04 +0000

    so far

---
+++ react-context/my-app/src/App.tsx
@@ -0,0 +1,35 @@
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
+import { About } from './pages/About';
+import { Catalog } from './pages/Catalog';
+import { NotFound } from './pages/NotFound';
+import { ProductDetails } from './pages/ProductDetails';
+import { CartContext, CartValue } from './components/CartContext';
+import { useState } from 'react';
+import { Product } from './lib';
+
+export function App() {
+  const [cart, setCart] = useState<Product[]>([]);
+
+  const addToCart = (product: Product) => {
+    setCart((prevCart) => [...prevCart, product]);
+  };
+
+  const CartContextValues: CartValue = {
+    cart,
+    addToCart,
+  };
+
+  return (
+    <CartContext.Provider value={CartContextValues}>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Catalog />} />
+          <Route path="details/:productId" element={<ProductDetails />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </CartContext.Provider>
+  );
+}
---
+++ react-context/my-app/src/components/CartContext.tsx
@@ -0,0 +1,14 @@
+import { Product } from '../lib';
+import { createContext } from 'react';
+
+export type CartValue = {
+  cart: Product[];
+  addToCart: (product: Product) => void;
+};
+
+const defaultCartValue: CartValue = {
+  cart: [],
+  addToCart: () => undefined,
+};
+
+export const CartContext = createContext<CartValue>(defaultCartValue);
